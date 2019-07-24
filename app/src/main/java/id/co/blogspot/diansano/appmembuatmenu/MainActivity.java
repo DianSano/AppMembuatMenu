2638e2dcedd2aac41568e6f88c8dcb45e54e079d
DianSano <alb.vds@gmail.com>
2019-07-24 11:35:19 +0700

    versi 2

--- app/src/main/java/id/co/blogspot/diansano/appmembuatmenu/MainActivity.java
+++ app/src/main/java/id/co/blogspot/diansano/appmembuatmenu/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Ini item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Ini item 2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem31:
+                Toast.makeText(this, "Ini sub item 3.1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem32:
+                Toast.makeText(this, "Ini sub item 3.2", Toast.LENGTH_SHORT).show();
+                return true;
+
+                default:return super.onOptionsItemSelected(item);
+        }
+
     }
 }
